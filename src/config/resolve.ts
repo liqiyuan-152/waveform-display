690075864baf3730ffa35332b03c82e700e8cee3
liqiyuan-152 <qy15283183170@gmail.com>
2026-08-24 21:30:05 +0800

    feat: resolve nested advanced options

--- src/config/resolve.ts
+++ src/config/resolve.ts
@@ -1,16 +1,25 @@
 import { defaultOptions } from './defaults'
 import type { WaveformOptions } from '../types/options'
 
-export function resolveOptions(options: WaveformOptions = {}): Required<WaveformOptions> {
+export function resolveOptions(options: WaveformOptions = {}) {
   return {
     ...defaultOptions,
     ...options,
+    responsive: { ...defaultOptions.responsive, ...options.responsive },
     padding: { ...defaultOptions.padding, ...options.padding },
-    frame: { ...defaultOptions.frame, ...options.frame },
+    frame: {
+      ...defaultOptions.frame,
+      ...options.frame,
+      top: { ...defaultOptions.frame.top, ...options.frame?.top },
+      right: { ...defaultOptions.frame.right, ...options.frame?.right },
+      bottom: { ...defaultOptions.frame.bottom, ...options.frame?.bottom },
+      left: { ...defaultOptions.frame.left, ...options.frame?.left },
+    },
     line: { ...defaultOptions.line, ...options.line },
     point: { ...defaultOptions.point, ...options.point },
-    xAxis: { ...defaultOptions.xAxis, ...options.xAxis },
-    yAxis: { ...defaultOptions.yAxis, ...options.yAxis },
+    area: { ...defaultOptions.area, ...options.area },
+    xAxis: { ...defaultOptions.xAxis, ...options.xAxis, title: { ...defaultOptions.xAxis.title, ...options.xAxis?.title } },
+    yAxis: { ...defaultOptions.yAxis, ...options.yAxis, title: { ...defaultOptions.yAxis.title, ...options.yAxis?.title } },
     grid: {
       ...defaultOptions.grid,
       ...options.grid,
