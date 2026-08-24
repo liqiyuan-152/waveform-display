f9becb84ace6568159965c67a4f9b110f633058e
liqiyuan-152 <qy15283183170@gmail.com>
2026-08-24 21:32:00 +0800

    refactor: add renderer context

---
+++ src/renderer/context.ts
@@ -0,0 +1,19 @@
+import type * as d3 from 'd3'
+import type { WaveformSeries } from '../types/data'
+import type { ReturnTypeOfResolveOptions } from '../types/internal'
+
+export interface RenderContext {
+  svg: d3.Selection<SVGSVGElement, unknown, null, undefined>
+  plot: d3.Selection<SVGGElement, unknown, null, undefined>
+  series: WaveformSeries[]
+  options: ReturnTypeOfResolveOptions
+  width: number
+  height: number
+  innerWidth: number
+  innerHeight: number
+  x: d3.ScaleLinear<number, number>
+  y: d3.ScaleLinear<number, number>
+  xDomain: [number, number]
+  yDomain: [number, number]
+  clipId: string
+}
